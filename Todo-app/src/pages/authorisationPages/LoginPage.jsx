fecd4ed62456b7eccb1d3b94170cd456587fcafe
byte <banan_2007@inbox.ru>
2025-06-05 21:16:56 +1000

    added login form logic

--- Todo-app/src/pages/authorisationPages/LoginPage.jsx
+++ Todo-app/src/pages/authorisationPages/LoginPage.jsx
@@ -1,16 +1,77 @@
 
 import { Link } from "react-router-dom"
+import './LoginForm.css'
+import './FontSize.css'
+import { useState } from "react"
 
-export default function LoginPage() {
+
+export default function LoginPage({ loginfunc }) {
+
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+
+    const handleUsername = (e) => {
+        preventDefault()
+        setUsername(e.target.value)
+    }
+
+    const handlePassword= (e) => {
+        preventDefault()
+        setPassword(e.target.value)
+    }
+
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        const getUserData = localStorage.getItem(username)
+
+        if (getUserData && getUserData === password) {
+            alert('Вы успешно вошли в систему!')
+            loginfunc()
+        } else {
+            alert('Данные введены неверно!')
+        }
+    }
 
     return (
         <>
-            <h1>Here our users will Login</h1>
-            <button>
-                <Link to='/registerPage'>
-                    регистрация
-                </Link>
-            </button>
+            <div className="limiter">
+                <div className="container-login100">
+                    <div className="wrap-login100 p-l-55 p-r-55 p-t-65 p-b-54">
+                        <form className="login100-form validate-form" onSubmit={handleSubmit}>
+                            <span className="login100-form-title p-b-49">
+                                Вход
+                            </span>
+
+                            <div className="wrap-input100 validate-input m-b-23" data-validate = "Username is reauired">
+                                <span className="label-input100">Имя пользователя</span>
+                                <input className="input100" onChange={setUsername} type="text" name="username" placeholder="Введите свое имя пользователя" />
+                            </div>
+
+                            <div className="wrap-input100 validate-input" data-validate="Password is required">
+                                <span className="label-input100">Пароль</span>
+                                <input className="input100" onChange={setPassword}  type="password" name="pass" placeholder="Введите свой пароль"/>
+                            </div>
+                            
+                            <div className="container-login100-form-btn">
+                                <div className="wrap-login100-form-btn">
+                                    <div className="login100-form-bgbtn"></div>
+                                    <button type="submit" className="login100-form-btn">
+                                        Войти
+                                    </button>
+                                </div>
+                            </div>
+
+
+                            <div className="flex-col-c p-t-155">
+                                <Link className="txt2" to='/registerPage'>
+                                    Еще нет аккаунта? - Регистрация
+                                </Link>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
